c6f8677a9e67f702c46b128d7de200d543f60c4e
Vicktor <133223399+vicktorviggo@users.noreply.github.com>
2025-06-21 00:24:26 +0200

    fixed directory

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'cerulean'
+
+games { 'gta5' }
+
+author 'vicktorviggo'
+
+description 'Custom native handlings'
+
+version '1.0'
+
+files {
+    'handling.meta',
+}
+
+data_file 'HANDLING_FILE' 'handling.meta'
